c2faf4db5fa706c3090939e5081c516fe3efcba3
matkronh <mats.kodehode@gmail.com>
2023-05-23 10:56:00 +0200

    s

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import styles from './styles/App.module.css'
+import headerLogo from './img/akks-holding-eiendom-as-high-resolution-logo-color-on-transparent-background.png'
+import bodyPicture from './img/avi-waxman-f9qZuKoZYoY-unsplash.jpg'
+import house1 from './img/house1.jpg'
+import house2 from './img/house2.jpg'
+import house3 from './img/house3.jpg'
+
+import { Parallax } from 'react-parallax';
+
+function App() {
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <img src={headerLogo} className={styles.logo}/>
+      </div>
+
+      <div className={styles.hero}>
+        <Parallax blur={0} bgImage={bodyPicture} bgImageAlt="sky photo over some houses" strength={500}>
+          <div className={styles.testdiv}>
+            <div className={styles.cardContainer}>
+              <div className={styles.card}>
+                <img src={house1} className={styles.cardHouse}/>
+                <p className={styles.cardPara}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ut quia sapiente ea minima.</p>
+                <h1 className={styles.cardTitle}>Buy a Home</h1>
+              </div>
+              <div className={styles.card}>
+                <img src={house2} className={styles.cardHouse}/>
+                <p className={styles.cardPara}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ut quia sapiente ea minima.</p>
+                <h1 className={styles.cardTitle}>Sell a Home</h1>
+              </div>
+              <div className={styles.card}>
+                <img src={house3} className={styles.cardHouse}/>
+                <p className={styles.cardPara}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ut quia sapiente ea minima.</p>
+                <h1 className={styles.cardTitle}>Rent a Home</h1>
+              </div>
+            </div>
+          </div>
+        </Parallax>
+      </div>
+      
+      <div className={styles.body}>
+
+      </div>
+
+      <div className={styles.footer}>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
