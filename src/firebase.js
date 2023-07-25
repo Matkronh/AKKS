366d7048532037396b54afb89dab26519e099bad
matkronh <mats.kodehode@gmail.com>
2023-07-25 15:03:33 +0200

    a

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-
+import key from '../key'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBjvXNvBDagb-3P4BwDi4CHC88SSSDcYZ4',
+  apiKey: key,
   authDomain: 'akksdev-e0197.firebaseapp.com',
   projectId: 'akksdev-e0197',
   databaseURL: 'https://akksdev-e0197-default-rtdb.europe-west1.firebasedatabase.app/'
